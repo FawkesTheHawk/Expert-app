3c7b7d67cce00b0753766ad5e48e8c201a36b981
Abazajian, Nico <nico.abazajian@rovisys.com>
2021-08-18 15:48:14 -0500

    bascis set up, now to create UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Expert-app';
-}
+  title = "Hello World";
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FilterComponent } from './filter/filter.component';
+import { ContentComponent } from './content/content.component';
+import { ScrollDirective } from './services/scrollListen.directive';
 
+import { CategoryService } from './services/category.service';
+import { CocktailService } from './services/cocktail.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilterComponent,
+    ContentComponent,
+    ScrollDirective,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
+  providers: [CategoryService, CocktailService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
